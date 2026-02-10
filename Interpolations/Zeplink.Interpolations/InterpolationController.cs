222ce185803a84610200492064d42f48c6e98c45
michelseb <sebastien.michel60@gmail.com>
2026-02-10 18:26:30 +0100

    Initial commit

---
+++ Interpolations/Zeplink.Interpolations/InterpolationController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZepLink.Interpolations
+{
+    public class InterpolationController : MonoBehaviour
+    {
+        public void Run<T, U>(Interpolation<T, U> interpolation) where T : Component where U : struct
+        {
+            StartCoroutine(InterpolationHelper<T, U>.Interpolate(interpolation));
+        }
+
+        public void RunList<T, U>(IList<Interpolation<T, U>> interpolations) where T : Component where U : struct
+        {
+            StartCoroutine(InterpolationHelper<T, U>.InterpolateList(interpolations));
+        }
+    }
+}
