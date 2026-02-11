1d03c77f9f13646d67b796dc2859f8f4133c54ae
michelseb <sebastien.michel60@gmail.com>
2026-02-11 16:26:01 +0100

    adding onEnd event to interpolationController

--- Zeplink.Interpolations/InterpolationController.cs
+++ Zeplink.Interpolations/InterpolationController.cs
@@ -1,23 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ZepLink.Interpolations
 {
     public class InterpolationController : MonoBehaviour
     {
-        public void Run(IInterpolation interpolation)
+        public void Run(IInterpolation interpolation, Action onEnd = null)
         {
-            StartCoroutine(InterpolationHelper.Interpolate(interpolation));
+            StartCoroutine(InterpolationHelper.Interpolate(interpolation, onEnd));
         }
 
-        public void RunListSequential(IList<IInterpolation> interpolations)
+        public void RunListSequential(IList<IInterpolation> interpolations, Action onEnd = null)
         {
-            StartCoroutine(InterpolationHelper.InterpolateListSequential(interpolations));
+            StartCoroutine(InterpolationHelper.InterpolateListSequential(interpolations, onEnd));
         }
 
-        public void RunListSimultaneous(IList<IInterpolation> interpolations)
+        public void RunListSimultaneous(IList<IInterpolation> interpolations, Action onEnd = null)
         {
-            StartCoroutine(InterpolationHelper.InterpolateListSimultaneous(interpolations));
+            StartCoroutine(InterpolationHelper.InterpolateListSimultaneous(interpolations, onEnd));
         }
     }
 }
