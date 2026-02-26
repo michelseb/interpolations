c94562b18d00c17616d69aad7d06e5f6103b50f0
michelseb <sebastien.michel60@gmail.com>
2026-02-27 00:55:44 +0100

    Adding tests

---
+++ Zeplink.Interpolations/Tests/DynamicInterpolationTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using ZepLink.Interpolations.Easings;
+
+namespace ZepLink.Interpolations.Impl
+{
+    [RequireComponent(typeof(InterpolationController))]
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class DynamicInterpolationTest : MonoBehaviour
+    {
+        private InterpolationController _controller;
+        private Camera _camera;
+        private SpriteRenderer _renderer;
+
+        [SerializeField] private EasingType _easing;
+        [SerializeField][Range(0, 2)] private float _animationDuration;
+
+        private void Awake()
+        {
+            _controller = GetComponent<InterpolationController>();
+            _renderer = GetComponent<SpriteRenderer>();
+            _camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var pos = _camera.ScreenToWorldPoint(Input.mousePosition);
+
+                _controller.Run(
+                    new TransformPositionInterpolation(transform, new Vector3(pos.x, pos.y), _animationDuration, _easing),
+                    () => { _renderer.color = Random.ColorHSV(); },
+                    true);
+            }
+        }
+    }
+}
